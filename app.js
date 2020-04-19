172869edb3549ab423fe20b504956087e4ca3e0d
KhanhHoang <Khanhsingapore@gmail.com>
2020-04-19 16:22:55 -0500

    code for DELETE

--- app.js
+++ app.js
@@ -46,11 +46,27 @@ app.get("/api/notes", function (req, res) {
 
 //POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 app.post('/api/notes', function (req, res){
-    var NewNote = {'id': Date.now(), ...req.body};
+    var NewNote = {'id':Math.floor(Math.random() * 1000), ...req.body};
     savedNotes.push(NewNote);
     WriteNote();
-    return res.send('note has been saved');
+    return res.send("Note has been saved");
 })
+
+// DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. 
+// This means you'll need to find a way to give each note a unique `id` when it's saved. 
+// In order to delete a note, you'll need to read all notes from the `db.json` file, 
+// remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+app.delete("/api/notes/:id", function (req, res) {
+    var SelectedNote = req.params.id;
+    console.log(SelectedNote);
+    savedNotes = savedNotes.filter(function(item){
+        return item.id != SelectedNote;
+    });
+    WriteNote();
+    return res.send("Note has been Deleted");
+});
+getNotes();
+
 
 // start server
 app.listen(PORT, function () {
