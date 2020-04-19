f20c404439897fdce16da638c8058012f2588eea
KhanhHoang <Khanhsingapore@gmail.com>
2020-04-19 14:59:48 -0500

    create HTML routes

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ var fs = require("fs");
 
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 5050;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
+//GET `*` - Should return the `index.html` file
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// GET `/notes` - Should return the `notes.html` file.
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
 
 // start server
 app.listen(PORT, function () {
